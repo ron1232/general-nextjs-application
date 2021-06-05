6a2ebbc86e71c55b78f6530d9f8e02a5aafbc7a9
Ron Barak <ron.b@hackerusa.com>
2021-06-05 23:45:44 +0300

    added pagination

---
+++ components/Pagination.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import { PER_PAGE } from '@/config/index';
+
+export default function Pagination({ page, total }) {
+  const pages = Math.ceil(total / PER_PAGE);
+  return (
+    <>
+      {[...Array(pages)].map((a, i) => (
+        <Link href={`/events?page=${i + 1}`}>
+          <a className={`btn-page ${page === i + 1 ? 'btn-page--active' : ''}`}>
+            {i + 1}
+          </a>
+        </Link>
+      ))}
+    </>
+  );
+}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,10 @@
 import Layout from '@/components/Layout';
 import EventItem from '@/components/EventItem';
-import { API_URL } from '@/config/index';
+import { API_URL, PER_PAGE } from '@/config/index';
 import http from '@/services/http';
+import Pagination from '@/components/Pagination';
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total }) {
   return (
     <Layout title='Home Page'>
       <h1>Events</h1>
@@ -12,15 +13,21 @@ export default function EventsPage({ events }) {
       ) : (
         events.map((evt) => <EventItem key={evt.id} evt={evt} />)
       )}
+      <Pagination page={page} total={total} PER_PAGE={PER_PAGE} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const { data: events } = await http(`${API_URL}/events?_sort=date:ASC`);
+export async function getServerSideProps({ query: { page = 1 } }) {
+  const { data: total } = await http(`${API_URL}/events/count`);
+
+  const { data: events } = await http(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${
+      (+page - 1) * PER_PAGE
+    }`
+  );
 
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events, page: +page, total },
   };
 }
